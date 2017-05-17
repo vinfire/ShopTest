d377681654b980e2447855b1660b1dceebc0f50d
Vinfire <vinfire4th@163.com>
2017-05-17 11:16:42 +0800

    Initial commit

---
+++ app/src/main/java/com/example/gtr/shoptest/utils/Constants.java
@@ -0,0 +1,16 @@
+package com.example.gtr.shoptest.utils;
+
+/**
+ * Created by GTR on 2017/5/17.
+ */
+
+public class Constants {
+    //Android Studio默认的模拟器使用这个IP
+    public static final String BASE = "http://10.0.2.2:80";
+
+    //请求Json数据基本URL
+    public static final String BASE_URL_JSON = BASE + "/atguigu/json/";
+    //请求图片基本URL
+    public static final String BASE_URL_IMAGE = BASE + "/atguigu/img/";
+
+}
